268d5339cc2a8f4fff8a10b980b6f02b24e2787b
Wei <75070418+wei-harness@users.noreply.github.com>
2022-11-21 16:31:02 +0000

    fix: Tutorial and Module Boxes - Text is only HREF - closes #DLP-49

--- src/components/LandingPage/ModuleCard.tsx
+++ src/components/LandingPage/ModuleCard.tsx
@@ -1,46 +1,67 @@
-import React from 'react';
+import React from "react";
 // import clsx from 'clsx';
-import Tooltip from 'rc-tooltip';
-import 'rc-tooltip/assets/bootstrap.css'
-import styles from './ModuleCard.module.scss';
-import Link from '@docusaurus/Link';
+import Tooltip from "rc-tooltip";
+import "rc-tooltip/assets/bootstrap.css";
+import styles from "./ModuleCard.module.scss";
+import Link from "@docusaurus/Link";
+import { FeatureItem } from "./AllTutorials";
 
-function Feature({title, ribbon, description, Svg, type, time, module, featureCard, link = "#"}: FeatureItem) {
+function Feature({
+  title,
+  ribbon,
+  description,
+  Svg,
+  type,
+  time,
+  module,
+  featureCard,
+  link = "#",
+}: FeatureItem) {
   return (
-    <div className={`${styles.tutorialCard} ${featureCard? styles.featureCard : styles[module]}`}>
-        <h6>{ Svg && 
-          <img src={Svg}/>
-        }{time}</h6>
-        { ribbon && <div className={styles.ribbon}>
-          <img src="/img/new.svg"/>
-        </div>}
-        <h4><Link to={link}>{title}</Link></h4>
+    <Link
+      to={link}
+      className={`${styles.tutorialCard} ${
+        featureCard ? styles.featureCard : ""
+      } ${styles[module]}`}
+    >
+      <div>
+        <h6>
+          {Svg && <img src={Svg} />}
+          {time}
+        </h6>
+        {ribbon && (
+          <div className={styles.ribbon}>
+            <img src="/img/new.svg" />
+          </div>
+        )}
+        <h4>{title}</h4>
         <p>{description}</p>
         <div>
-        <ul className={styles.docTypes}>
-          {type.map((props, idx) => (
+          <ul className={styles.docTypes}>
+            {type.map((props, idx) => (
               <li>
                 <Tooltip placement="top" overlay={props}>
                   <img src={`/img/icon_doctype_${props}.svg`} alt={props} />
                 </Tooltip>
               </li>
-            ))}          
-        </ul>
+            ))}
+          </ul>
+        </div>
       </div>
-    </div>
+    </Link>
   );
 }
 
 export default function ModuleCard(props): JSX.Element {
   return (
-      <div className={styles.spaceBetween}>        
-          {props.featureCard ? props.FeatureList.map((props, idx) => (
-            <Feature key={idx} {...props} featureCard={true}/>
-          )) :
-          props.FeatureList.map((props, idx) => (
-            <Feature key={idx} {...props} featureCard={false}/>
+    <div className={styles.spaceBetween}>
+      {props.featureCard
+        ? props.FeatureList.map((props, idx) => (
+            <Feature key={idx} {...props} featureCard={true} />
           ))
-          }
-      </div>
+        : props.FeatureList.map((props, idx) => (
+            <Feature key={idx} {...props} featureCard={false} />
+          ))}
+    </div>
   );
 }
