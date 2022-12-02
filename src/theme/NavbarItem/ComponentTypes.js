509746b84bcf2983a2671c22dcaf00045f4520ed
Wei <75070418+wei-harness@users.noreply.github.com>
2022-12-02 00:48:09 +0000

    chore: format css content

--- src/theme/NavbarItem/ComponentTypes.js
+++ src/theme/NavbarItem/ComponentTypes.js
@@ -3,5 +3,6 @@ import CoveoSearch from "@site/src/components/NavbarItems/CoveoSearch";
 
 export default {
   ...ComponentTypes,
+  // add CoveoSearch as a navbar item
   "custom-coveo-search": CoveoSearch,
 };
