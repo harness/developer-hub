c127d2cb1c6bd1aa65886d1c57fcc4de01077813
Ravi Lachhman <ravi@lachhman.io>
2022-12-12 21:24:42 -0500

    modified landing page links
    Modified landing page links.

--- src/components/LandingPage/ContinuousIntegration.tsx
+++ src/components/LandingPage/ContinuousIntegration.tsx
@@ -20,7 +20,7 @@ const FeaturedList: CardItem[] = [
     newDoc: true,
     type: [docType.Documentation],
     time: "5 min",
-    link: "/tutorials/build-code/fastest-ci-tool",
+    link: "/tutorials/build-code/fastest-ci",
   },
   {
     title: "Node and Docker CI Pipeline",
@@ -126,7 +126,7 @@ const CIList: CardItem[] = [
     newDoc: true,
     type: [docType.Documentation],
     time: "5 min",
-    link: "/tutorials/build-code/fastest-ci-tool",
+    link: "/tutorials/build-code/fastest-ci",
   },
 ];
 
