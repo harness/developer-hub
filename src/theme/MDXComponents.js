20ff2ff5adb596462517aceb7f62b7eadc939c58
Wei <75070418+wei-harness@users.noreply.github.com>
2022-12-16 21:00:14 +0000

    feat: CTA Banner component

--- src/theme/MDXComponents.js
+++ src/theme/MDXComponents.js
@@ -2,6 +2,7 @@
 // Import the original mapper
 import MDXComponents from "@theme-original/MDXComponents";
 import DocVideo from "@site/src/components/DocVideo";
+import CTABanner from "@site/src/components/CTABanner";
 
 export default {
   // Re-use the default mapping
@@ -9,4 +10,5 @@ export default {
   // Map the "highlight" tag to our <Highlight /> component!
   // `Highlight` will receive all props that were passed to `highlight` in MDX
   docvideo: DocVideo,
+  ctabanner: CTABanner,
 };
