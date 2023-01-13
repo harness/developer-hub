9612b9e2dc51a0ffd14eab5e5af686d1de92f607
Ravi Lachhman <ravi@lachhman.io>
2023-01-13 10:26:56 -0500

    Added scaffolding for re-directs

--- client-redirects.js
+++ client-redirects.js
@@ -1,5 +1,6 @@
 module.exports = {
   redirects: [
+    //Static Re-directs
     {
       from: "/release-notes",
       to: "/release-notes/whats-new",
@@ -8,5 +9,12 @@ module.exports = {
       from: "/docs",
       to: "/docs/category/documentation",
     },
+    //Temporary Re-Directs
+
+    //Created by User for PR-123 on Jan 14, 2023
+    // {
+    //   from: "/docs/platform/delegates/delegate-guide/automate-delegate-installation",
+    //   to: "docs/platform/delegates/installation",
+    // },
   ],
 };
