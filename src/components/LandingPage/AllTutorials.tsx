62670d3fb18d8c63d6a9a88e1a4c700a26eacdf6
Pavan Belagatti <22288065+pavanbelagatti@users.noreply.github.com>
2023-02-13 20:13:41 +0530

    Adding the 'Customize Delegate' tutorial

--- src/components/LandingPage/AllTutorials.tsx
+++ src/components/LandingPage/AllTutorials.tsx
@@ -372,18 +372,6 @@ const STOList: CardItem[] = [
     time: "10min",
     link: "/tutorials/orchestrate-security-tests/nodejs-firstscan",
   },
-  {
-    title: "Integrating Synk and OWASP Scan for NodeJS Application",
-    module: "sto",
-    icon: "img/icon_sto.svg",
-    description: (
-      <>Scanning a NodeJS Application with Snyk and Owasp for vulnerabilities.</>
-    ),
-    newDoc: false,
-    type: [docType.Documentation],
-    time: "15min",
-    link: "/tutorials/orchestrate-security-tests/integrating-snyk-and-owasp-scan-for-nodejs-app",
-  },
 ];
 
 const CEList: CardItem[] = [
