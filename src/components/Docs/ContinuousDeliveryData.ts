0b150248e505dda50885d6f12e50d6d8ae990cb3
Ravi Lachhman <ravi@lachhman.io>
2023-03-07 18:05:25 -0500

    Modified Tiles to match existing Category
    * Modified Tiles to match existing Category
    * Archived the Complex Example

--- src/components/Docs/ContinuousDeliveryData.ts
+++ src/components/Docs/ContinuousDeliveryData.ts
@@ -32,133 +32,72 @@ export const featuredTutorials: CardItem[] = [
   // Docs
   export const docsCards: CardSections = [
     {
-      name: "Onboard with Continuous Delivery",
+      name: "Continuous Delivery & GitOps Docs",
       description:
-        "This topic covers CD Pipeline basics to get you ready to start building Pipelines easily.",
+        "These are prudent topics to Continuous Delivery.",
       list: [
         {
-          title: "CD overview and key concepts",
+          title: "Onboard with Continuous Delivery",
           module: "cd",
           description:
-            "This topic covers CD Pipeline basics to get you ready to start building Pipelines easily.",
-          link: "/docs/continuous-delivery/onboard-cd/cd-concepts/cd-pipeline-basics",
+            "This topic covers onboarding with Continuous Delivery.",
+          link: "/docs/category/onboard-with-continuous-delivery",
         },
         {
-          title: "CD pipeline modeling overview",
+          title: "Services",
           module: "cd",
           description:
-            "This topic describes how you use the Harness Continuous Delivery Abstraction Model.",
-          link: "/docs/continuous-delivery/onboard-cd/cd-concepts/cd-pipeline-modeling-overview",
+            "This topic covers Services.",
+          link: "/docs/category/services",
         },
         {
-          title: "Services and environments overview",
+          title: "Infrastructure",
           module: "cd",
           description:
-            "This topic describes Harness Continuous Delivery (CD) services and environments.",
-          link: "/docs/continuous-delivery/onboard-cd/cd-concepts/services-and-environments-overview",
+            "This topic covers Infrastructure.",
+          link: "/docs/category/infrastructure",
         },
-      ],
-    },
-    {
-      name: "Services",
-      list: [
         {
-          title: "Add Inline Service Files Using File Store",
+          title: "Execution",
           module: "cd",
           description:
-            "You can use Kubernetes manifests and other configuration files in Git repos or in the Harness File Store.",
-          link: "/docs/continuous-delivery/cd-services/cd-services-general/add-inline-manifests-using-file-store",
+            "This topic covers Execution.",
+          link: "/docs/category/execution",
         },
         {
-          title: "Propagate CD services",
+          title: "Deployments",
           module: "cd",
           description:
-            "This topic describes how to propagate CD services between stages.",
-          link: "/docs/continuous-delivery/cd-services/cd-services-general/propagate-and-override-cd-services",
+            "This topic covers Deployments.",
+          link: "/docs/category/deployments",
         },
-      ],
-    },
-    {
-      name: "Infrastructure",
-      list: [
         {
-          title:
-            "Provision Target Deployment Infra Dynamically with CloudFormation",
+          title: "GitOps",
           module: "cd",
           description:
-            "This topic describes how to provision a CD stage's deployment infrastructure resources using the CloudFormation Create Stack, Delete Stack, and Rollback Stack steps.",
-          link: "/docs/continuous-delivery/cd-infrastructure/cloudformation-infra/provision-target-deployment-infra-dynamically-with-cloud-formation",
+            "This topic covers GitOps.",
+          link: "/docs/category/gitops",
         },
-      ],
-    },
-    {
-      name: "Execution",
-      list: [
         {
-          title: "Using HTTP Requests in CD Pipelines",
+          title: "Advanced CD",
           module: "cd",
           description:
-            "You can use the HTTP step to run HTTP methods containing URLs, methods, headers, assertions, and variables. It helps you avoid having script cURL commands for simple REST calls.",
-          link: "/docs/continuous-delivery/cd-execution/cd-general-steps/using-http-requests-in-cd-pipelines",
+            "This topic covers Advanced CD.",
+          link: "/docs/category/advanced-cd",
         },
-      ],
-    },
-    {
-      name: "Deployments",
-      list: [
         {
-          title: "Deployment concepts and strategies",
+          title: "CD Dashboards",
           module: "cd",
           description:
-            "You have likely heard terms like blue/green and canary when it comes to deploying code and applications into production. ",
-          link: "/docs/continuous-delivery/cd-deployments-category/deployment-concepts",
+            "This topic covers CD Dashboards.",
+          link: "/docs/category/cd-dashboards",
         },
-      ],
-    },
-    {
-      name: "GitOps",
-      list: [
         {
-          title: "Harness GitOps Basics",
+          title: "CD Tech Reference",
           module: "cd",
           description:
-            "This topic describes the basic concepts of Harness GitOps.",
-          link: "/docs/continuous-delivery/cd-gitops/harness-git-ops-basics",
-        },
-      ],
-    },
-    {
-      name: "Advanced CD",
-      list: [
-        {
-          title: "Kubernetes Deployments Basics",
-          module: "cd",
-          description:
-            "This topic describes the concept of a Harness Kubernetes deployment by describing the high-level steps involved.",
-          link: "/docs/continuous-delivery/cd-advanced/cd-kubernetes-category/kubernetes-deployments-overview",
-        },
-      ],
-    },
-    {
-      name: "CD Dashboards",
-      list: [
-        {
-          title: "Monitor Deployments and Services in CD Dashboards",
-          module: "cd",
-          description:
-            "This topic describes how you can use DORA and other advanced metrics for deployments and services in CD Dashboards.",
-          link: "/docs/continuous-delivery/cd-dashboards/monitor-cd-deployments",
-        },
-      ],
-    },
-    {
-      name: "CD Tech Reference",
-      list: [
-        {
-          title: "HTTP Step Reference",
-          module: "cd",
-          description: "This topic describes the settings for the HTTP step.",
-          link: "/docs/continuous-delivery/cd-technical-reference/cd-gen-ref-category/http-step",
+            "This topic covers CD Tech References.",
+          link: "/docs/category/cd-tech-reference",
         },
       ],
     },
