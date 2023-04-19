199dfa24a109bdd88f924949947a15ecd8901134
SmritiSatya <smriti.satyanarayana@harness.io>
2023-04-19 20:42:16 +0530

    Fix build error
    Fix build error

--- src/components/Docs/CloudCostManagement.tsx
+++ src/components/Docs/CloudCostManagement.tsx
@@ -5,7 +5,7 @@ import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
 import styles from "./styles.module.scss";
 import TutorialCard, { TutorialCards } from "../LandingPage/TutorialCard";
 // Define the cards in "***Data.ts"
-import { featuredTutorials, docsCards } from "./data/cloudCostManagementData";
+import { docsCards } from "./data/cloudCostManagementData";
 
 export default function CCM() {
   const { siteConfig: { baseUrl = "/" } = {} } = useDocusaurusContext();
