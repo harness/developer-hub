d1b73e3cacdb00fae3002b3f8a8cbcb778d46a6f
Sudheendra Katte <sudheendra.katte@harness.io>
2023-04-27 20:57:30 +0530

    cet hdh module

---
+++ src/components/Docs/ContinuousErrorTracking.tsx
@@ -0,0 +1,62 @@
+import React from "react";
+import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
+import Link from "@docusaurus/Link";
+import clsx from "clsx";
+import styles from "./styles.module.scss";
+import TutorialCard, { TutorialCards } from "../LandingPage/TutorialCard";
+// Define the cards in "***Data.ts"
+import {
+  featuredTutorials,
+  docsCards,
+} from "./data/continuousErrorTrackingData";
+
+export default function CET() {
+  const { siteConfig: { baseUrl = "/" } = {} } = useDocusaurusContext();
+  return (
+    <div className="container">
+      <div className={styles.topSection}>
+        <div className={styles.spaceBetween}>
+          <div className={styles.moduleTitle}>
+            <img src={`${baseUrl}img/icon_cet.svg`} />
+            <h1>Continuous Error Tracking</h1>
+          </div>
+          <div className={styles.btnContainer}>
+            <Link href="/tutorials/service-reliability">
+              <button className={styles.btn}>
+                {/* <i className="fa-regular fa-file"></i> */}
+                <img src={`${baseUrl}img/icon_tutorials.svg`} />
+                Tutorials
+              </button>
+            </Link>
+            <Link href="/release-notes/service-reliability-management">
+              <button className={styles.btn}>
+                {/* <i className="fa-regular fa-file"></i> */}
+                <img src={`${baseUrl}img/icon_release_notes.svg`} />
+                Release Notes
+              </button>
+            </Link>
+          </div>
+        </div>
+        <div className={styles.spaceBetween}>
+          <div className={styles.content}>
+            <p>
+              Learn how you can identify, triage, and resolve errors in
+              applications with Harness CET.
+            </p>
+          </div>
+        </div>
+      </div>
+      <TutorialCards data={docsCards} sectionClass={styles.subSection} />
+      {featuredTutorials && featuredTutorials.length > 0 && (
+        <>
+          <div className={styles.sectionDivider}></div>
+          <div className={styles.subSection}>
+            <h3>Featured Tutorials</h3>
+            <TutorialCard FeatureList={featuredTutorials} featuredCard={true} />
+          </div>
+        </>
+      )}
+    </div>
+    // </Layout>
+  );
+}
