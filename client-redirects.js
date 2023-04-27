ccb8024f446bff049f3c6c4e5867004d3f7fcbf2
rashminandasahoo <rashmi.nandasahoo@harness.io>
2023-04-27 09:13:10 +0530

    Added redirects for PL links given by Kim

--- client-redirects.js
+++ client-redirects.js
@@ -34,6 +34,32 @@ module.exports = {
     //  },
     //===================================================================================
 
+    // Created by Rashmi Sahoo to fix PL redirects April 27th, 2023
+    {
+      from: "/docs/category/role-based-access-control",
+      to: "/docs/category/access-control-1",
+    },
+
+    {
+      from: "/docs/platform/Delegates/delegate-concepts/delegate-overview.md",
+      to: "/docs/platform/Delegates/delegate-concepts/delegate-overview"
+    },
+
+    {
+      from: "/docs/platform/delegates/delegate-install-kubernetes/install-harness-delegate-on-kubernetes/",
+      to: "/docs/platform/Delegates/install-delegates/install-a-kubernetes-delegate",
+    },
+
+    {
+      from: "/docs/platform/Governance/Policy-as-code/docs/feature-flags/harness-policy-engine",
+      to: "/docs/feature-flags/harness-policy-engine"
+    },
+
+    {
+      from: "/tutorials/platform/gke-workload-identity/",
+      to: "/tutorials/platform/gke-workload-identity",
+    },
+
     // Created by ravilach to move CVKB into Docs April 21st, 2023
     {
       from: "/kb/continuous-delivery/continuous-verification-ml",
