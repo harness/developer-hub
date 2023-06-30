784c29fe8694d5c98693cda7933843dd2779dcab
Wei <75070418+wei-harness@users.noreply.github.com>
2023-06-30 20:12:38 +0100

    chore: rename hdh logo

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -79,7 +79,7 @@ const config = {
         title: "Harness Developer Hub",
         logo: {
           alt: "Harness Developer Hub",
-          src: "img/logo.svg",
+          src: "img/logo_hdh.svg",
         },
         items: [
           /*{
