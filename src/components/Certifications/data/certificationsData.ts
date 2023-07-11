42708388212274d7ddffbb0773b3d84fdadb43e9
Rohan Maharjan <rohanmrzan100@gmail.com>
2023-07-11 20:09:13 +0545

    edited certifications and added new accessibility page

--- src/components/Certifications/data/certificationsData.ts
+++ src/components/Certifications/data/certificationsData.ts
@@ -1,56 +1,115 @@
-
 import { certType, CardItem } from "../CertCard";
-import { MODULES } from "../../../constants"
+import { MODULES } from "../../../constants";
 
 export const certifications: CardItem[] = [
-    {
-      title: "Continuous Delivery & GitOps - Developer",
-      module: MODULES.cd,
-      type: certType.developer,
-      description:
-        "Continuous Delivery & GitOps focuses on delivery and deployment of application and infrastructure changes in a safe and sustainable way. Your Continuous Delivery pipeline should automate all of the steps necessary to get your changes into production.",
-      version: "Harness CD & GitOps Free/Team Plans",
-      link: "/certifications/continuous-delivery?lvl=developer",
-      // numberOfCerts: 1,
-    },
-    {
-      title: "Continuous Delivery & GitOps - Administrator",
-      module: MODULES.cd,
-      type: certType.administrator,
-      description:
-        "CD Administrator focuses on delivery and deployment of application and infrastructure changes in a safe and sustainable way. Your Continuous Delivery pipeline should automate all of the steps necessary to get your changes into production.",
-      version: "Harness CD & GitOps Enterprise Plan",
-      link: "/certifications/continuous-delivery?lvl=administrator",
-      // numberOfCerts: 1,
-    },
-    {
-      title: "Continuous Delivery & GitOps - Architect (BETA COMING SOON)",
-      module: MODULES.cd,
-      type: certType.architect,
-      description:
-        "CD Architect focuses on delivery and deployment of application and infrastructure changes in a safe and sustainable way. Your Continuous Delivery pipeline should automate all of the steps necessary to get your changes into production.",
-      version: "Harness CD & GitOps Enterprise Plan",
-      link: "/certifications/continuous-delivery?lvl=architect",
-      // numberOfCerts: 1,
-    },
-    {
-      title: "Continuous Integration - Developer",
-      module: MODULES.ci,
-      type: certType.developer,
-      description:
-        "Continuous Integration focuses on building and testing your code. Your Continuous Integration pipeline should provides a bird's-eye view of all your builds and drill down into specific builds to troubleshoot and analyze the root causes of issues.",
-      version: "Harness CI Free/Team Plans",
-      link: "/certifications/continuous-integration?lvl=developer",
-      // numberOfCerts: 1,
-    },
-    {
-      title: "Continuous Integration - Administrator (BETA COMING SOON)",
-      module: MODULES.ci,
-      type: certType.administrator,
-      description:
-        "CI Administrator focuses on building and testing your code at scale. Your Continuous Integration pipeline should automate all of the steps necessary to build and test your artifacts at scale.",
-      version: "Harness CI Enterprise Plan",
-      link: "/certifications/continuous-integration?lvl=administrator",
-      // numberOfCerts: 1,
-    },
-  ];
+  {
+    title: "Continuous Delivery & GitOps - Developer",
+    module: MODULES.cd,
+    type: certType.developer,
+    description:
+      "Continuous Delivery & GitOps focuses on delivery and deployment of application and infrastructure changes in a safe and sustainable way. Your Continuous Delivery pipeline should automate all of the steps necessary to get your changes into production.",
+    version: "Harness CD & GitOps Free/Team Plans",
+    link: "/certifications/continuous-delivery?lvl=developer",
+    // numberOfCerts: 1,
+  },
+  {
+    title: "Continuous Delivery & GitOps - Administrator",
+    module: MODULES.cd,
+    type: certType.administrator,
+    description:
+      "CD Administrator focuses on delivery and deployment of application and infrastructure changes in a safe and sustainable way. Your Continuous Delivery pipeline should automate all of the steps necessary to get your changes into production.",
+    version: "Harness CD & GitOps Enterprise Plan",
+    link: "/certifications/continuous-delivery?lvl=administrator",
+    // numberOfCerts: 1,
+  },
+  {
+    title: "Continuous Delivery & GitOps - Architect (BETA COMING SOON)",
+    module: MODULES.cd,
+    type: certType.architect,
+    description:
+      "CD Architect focuses on delivery and deployment of application and infrastructure changes in a safe and sustainable way. Your Continuous Delivery pipeline should automate all of the steps necessary to get your changes into production.",
+    version: "Harness CD & GitOps Enterprise Plan",
+    link: "/certifications/continuous-delivery?lvl=architect",
+    // numberOfCerts: 1,
+  },
+  {
+    title: "Continuous Integration - Developer",
+    module: MODULES.ci,
+    type: certType.developer,
+    description:
+      "Continuous Integration focuses on building and testing your code. Your Continuous Integration pipeline should provides a bird's-eye view of all your builds and drill down into specific builds to troubleshoot and analyze the root causes of issues.",
+    version: "Harness CI Free/Team Plans",
+    link: "/certifications/continuous-integration?lvl=developer",
+    // numberOfCerts: 1,
+  },
+  {
+    title: "Continuous Integration - Administrator (BETA COMING SOON)",
+    module: MODULES.ci,
+    type: certType.administrator,
+    description:
+      "CI Administrator focuses on building and testing your code at scale. Your Continuous Integration pipeline should automate all of the steps necessary to build and test your artifacts at scale.",
+    version: "Harness CI Enterprise Plan",
+    link: "/certifications/continuous-integration?lvl=administrator",
+    // numberOfCerts: 1,
+  },
+  {
+    title: "Feature Flag Certifications",
+    module: MODULES.ff,
+    type: certType.developer,
+    description:
+      "Continuous Delivery & GitOps focuses on delivery and deployment of application and infrastructure changes in a safe and sustainable way. ",
+    // version: "Harness CI Free/Team Plans",
+    // link: "/certifications/continuous-integration?lvl=developer",
+    // numberOfCerts: 1,
+  },
+  {
+    title: "Cloud Cost Management Certifications",
+    module: MODULES.ccm,
+    type: certType.developer,
+    description:
+      "Continuous Delivery & GitOps focuses on delivery and deployment of application and infrastructure changes in a safe and sustainable way. ",
+    // version: "Harness CI Free/Team Plans",
+    // link: "/certifications/continuous-integration?lvl=developer",
+    // numberOfCerts: 1,
+  },
+  {
+    title: "Service Reliability Management Certifications",
+    module: MODULES.srm,
+    type: certType.developer,
+    description:
+      "Continuous Delivery & GitOps focuses on delivery and deployment of application and infrastructure changes in a safe and sustainable way. ",
+    // version: "Harness CI Free/Team Plans",
+    // link: "/certifications/continuous-integration?lvl=developer",
+    // numberOfCerts: 1,
+  },
+  {
+    title: "Security Testing Orchestration Certifications",
+    module: MODULES.sto,
+    type: certType.developer,
+    description:
+      "Continuous Delivery & GitOps focuses on delivery and deployment of application and infrastructure changes in a safe and sustainable way. ",
+    // version: "Harness CI Free/Team Plans",
+    // link: "/certifications/continuous-integration?lvl=developer",
+    // numberOfCerts: 1,
+  },
+  {
+    title: "Chaos Engineering Certifications",
+    module: MODULES.ce,
+    type: certType.developer,
+    description:
+      "Continuous Delivery & GitOps focuses on delivery and deployment of application and infrastructure changes in a safe and sustainable way. ",
+    // version: "Harness CI Free/Team Plans",
+    // link: "/certifications/continuous-integration?lvl=developer",
+    // numberOfCerts: 1,
+  },
+  {
+    title: "Software Engineering Insights Certifications",
+    module: MODULES.sei,
+    type: certType.developer,
+    description:
+      "Continuous Delivery & GitOps focuses on delivery and deployment of application and infrastructure changes in a safe and sustainable way. ",
+    // version: "Harness CI Free/Team Plans",
+    // link: "/certifications/continuous-integration?lvl=developer",
+    // numberOfCerts: 1,
+  },
+];
--- src/pages/legal/accessibility.tsx
+++ src/pages/legal/accessibility.tsx
@@ -2,10 +2,10 @@ import React from "react";
 import MDXContent from "@theme/MDXContent";
 import Layout from "@theme/Layout";
 import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
-import Accessibility from "../../../docs/legal/accessibility.md";
 import styles from "./accessibility.module.scss";
 
 export default function AccessibilityPage() {
+  // const { siteConfig: { baseUrl = "/" } = {} } = useDocusaurusContext();
   const { siteConfig } = useDocusaurusContext();
   return (
     <div>
@@ -15,7 +15,82 @@ export default function AccessibilityPage() {
           description={`${siteConfig.tagline}`} // "Description will go into a meta tag in <head />"
         >
           <main className={`container ${styles.container}`}>
-            <Accessibility />
+            <h1>Harness Accessibility</h1>
+            <h2>Our Accessibility Philosophy</h2>
+            <p>
+              At Harness, we are committed to making our platform accessible to
+              everyone, drawing inspiration from Universal Design. Universal
+              Design is a philosophy that aims to create inclusive products and
+              systems, regardless of abilities or disabilities, ensuring equal
+              access and opportunity for all.
+            </p>
+            <p>
+              While we are still early in our accessibility journey, we are
+              dedicated to achieving this goal through the following areas:
+            </p>
+            <div className={styles.goals}>
+              <div className={styles.modules}>
+                <div>
+                  <img
+                    src={`/img/accessibility/accessibility_img_1.svg`}
+                    alt=""
+                  />
+                  <h4>Making all modules & features accessible</h4>
+                  <p>
+                    We strive to make every Harness module and feature easy to
+                    access and use for everyone.
+                  </p>
+                </div>
+              </div>
+              <div className={styles.modules}>
+                <div>
+                  <img
+                    src={`/img/accessibility/accessibility_img_2.svg`}
+                    alt=""
+                  />
+                  <h4>Accessibility at the core of the business</h4>
+                  <p>
+                    We prioritize accessibility at the heart of our business,
+                    ensuring it is an integral part of our values and
+                    operations.
+                  </p>
+                </div>
+              </div>
+              <div className={styles.modules}>
+                <div>
+                  <img
+                    src={`/img/accessibility/accessibility_img_3.svg`}
+                    alt=""
+                  />
+                  <h4>Embracing an accessibility mindset</h4>
+                  <p>
+                    We integrate our accessibility philosophy into the daily
+                    work of every Harness employee, fostering a culture of
+                    inclusivity.
+                  </p>
+                </div>
+              </div>
+            </div>
+            <p>
+              Through these efforts, our aim is to meet the AA accessibility
+              standard. If you encounter any accessibility issues or need
+              further assistance with our modules, please don't hesitate to
+              reach out to us at accessibility@harness.io.
+            </p>
+            <p>
+              You can find our latest VPAT reports for each module and the
+              platform using the links below:
+            </p>
+            <ul>
+              <li>Harness CD VPAT Report</li>
+              <li>Harness CI VPAT Report</li>
+              <li>Harness FF VPAT Report</li>
+              <li>Harness CCM VAT Report</li>
+              <li>Harness STO VPAT Report</li>
+              <li>Harness SRM VPAT Report</li>
+              <li> Harness Chaos VPAT Report</li>
+              <li>Harness Platform VPAT Report</li>
+            </ul>
           </main>
         </Layout>
       </MDXContent>
