cf3560f4aee791c3cbd51e02cf0e039029d26946
Rohan Maharjan <rohan.maharjan@harness.io>
2023-07-20 22:31:26 +0545

    made icon optional and made button inline

--- release-notes/continuous-delivery.md
+++ release-notes/continuous-delivery.md
@@ -13,7 +13,6 @@ import TabItem from '@theme/TabItem';
 
 <DocsButton icon = "fa-solid fa-square-rss" text="Subscribe via RSS" link="/release-notes/continuous-delivery/rss.xml" />
 
-
 Review the notes below for details about recent changes to Harness Continuous Delivery & GitOps (NextGen SaaS). For release notes for Harness Self-Managed Enterprise Edition, go to [Self-Managed Enterprise Edition release notes](/release-notes/self-managed-enterprise-edition). For FirstGen release notes, go to [Harness SaaS Release Notes (FirstGen)](/docs/first-gen/firstgen-release-notes/harness-saa-s-release-notes). Additionally, Harness publishes security advisories for every release. Go to the [Harness Trust Center](https://trust.harness.io/?itemUid=c41ff7d5-98e7-4d79-9594-fd8ef93a2838&source=documents_card) to request access to the security advisories.
 
 :::info note
--- src/components/DocsButton/index.tsx
+++ src/components/DocsButton/index.tsx
@@ -7,31 +7,31 @@ import Tooltip from "rc-tooltip";
 interface Props {
   text: string;
   link: string;
-  icon: string;
+  icon?: string;
   tooltip?: string;
   size?: string;
 }
 const DocsButton: React.FC<Props> = (props) => {
-  const [size, setSize] = useState<string | null>(props.size);
+  const size: string | null = props.size;
   const { siteConfig } = useDocusaurusContext();
   const baseUrl = siteConfig.url;
 
   return (
-    <Link to={baseUrl + props.link} className="link">
-      {props.tooltip ? (
-        <Tooltip placement="top" overlay={<span>{props.tooltip}</span>}>
-          <button className={`doc-button  ${size ? size : "small"}`}>
+      <Link to={baseUrl + props.link} className="link">
+        {props.tooltip ? (
+          <Tooltip placement="top" overlay={<span>{props.tooltip}</span>}>
+            <button className={`doc-button  ${size ? size : "small"}`}>
+              {props.text}
+              {props.icon && <i className={`${props.icon} custom-icon`}></i>}
+            </button>
+          </Tooltip>
+        ) : (
+          <button className={`doc-button ${size ? size : "small"}`}>
             {props.text}
-            <i className={`${props.icon} custom-icon`}></i>
+            {props.icon && <i className={`${props.icon} custom-icon`}></i>}
           </button>
-        </Tooltip>
-      ) : (
-        <button className={`doc-button ${size ? size : "small"}`}>
-          {props.text}
-          <i className={`${props.icon} custom-icon`}></i>
-        </button>
-      )}
-    </Link>
+        )}
+      </Link>
   );
 };
 
