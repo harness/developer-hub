e062037d0255dbea97b04b361bc8caafec54da55
Rohan Maharjan <rohan.maharjan@harness.io>
2023-08-30 22:48:21 +0545

    removed shadow from pill

--- src/components/DocsTag/index.tsx
+++ src/components/DocsTag/index.tsx
@@ -11,6 +11,13 @@ interface Props {
   textColor?: string;
   iconColor?: string;
 }
+// use it like this
+//  <DocsTag icon = "fa-solid fa-icons" text="Default" link="/docs/security-testing-orchestration" />
+//  <DocsTag  text="Default without icon" link="/docs/security-testing-orchestration" />
+//  <DocsTag  backgroundColor= "#ff8ac1" text="With out icon"  textColor="#ca136c"  />
+//  <DocsTag icon = "fa-solid fa-hand-dots"  backgroundColor= "#cbe2f9"   textColor="#0b5cad" iconColor="#6938c0" text="Community" link="/docs/security-testing-orchestration"  />
+
+
 const DocsTag: React.FC<Props> = (props) => {
   const { siteConfig } = useDocusaurusContext();
   const baseUrl = siteConfig.url;
