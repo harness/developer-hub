3ce67c6861e4b9a853e31912ceacad68935b8a6b
Rohan Maharjan <rohan.maharjan@harness.io>
2023-09-22 13:47:52 +0545

    HDH-179

--- src/components/HomepageCertifications/data/certificationsData.ts
+++ src/components/HomepageCertifications/data/certificationsData.ts
@@ -1,5 +1,3 @@
-
-
 import { MODULES } from "../../../constants";
 import  { CardItem } from "../../Certifications/CertCard";
 // Define the certs section cards here
@@ -20,27 +18,27 @@ export const certifications: CardItem[] = [
     link: "/certifications/continuous-integration",
     numberOfCerts: 3,
   },
-  // {
-  //   title: "Feature Flags Certifications",
-  //   module: MODULES.ff,
-  //   description:
-  //     "Drastically reduce rollbacks, deploy faster with less risk,spend more time building and coding, and less time fixing production with Progressive Delivery.",
-  //   link: "/certifications/feature-flags",
-  //   numberOfCerts: 3,
-  // },
-  // {
-  //   title: "Cloud Cost Management Certifications",
-  //   module: MODULES.ccm,
-  //   description:
-  //     " Save time, reduce effort, and save on your cloud bill with intelligent cloud cost automation.Detect and stop cloud cost anomalies as they occur, to avoid unpleasant billing surprises with a FinOps approach.",
-  //   link: "/certifications/cloud-cost-management",
-  //   numberOfCerts: 3,
-  // },
-  // {
-  //   title: "Chaos Engineering Certifications",
-  //   module: MODULES.ce,
-  //   description: "Comming Soon",
-  //   link: "/certifications/chao-engineering",
-  //   numberOfCerts: 3,
-  // },
+  {
+    title: "Feature Flags Certifications",
+    module: MODULES.ff,
+    description:
+      "Drastically reduce rollbacks, deploy faster with less risk,spend more time building and coding, and less time fixing production with Progressive Delivery.",
+    link: "/certifications/feature-flags",
+    numberOfCerts: 3,
+  },
+  {
+    title: "Cloud Cost Management Certifications",
+    module: MODULES.ccm,
+    description:
+      " Save time, reduce effort, and save on your cloud bill with intelligent cloud cost automation.Detect and stop cloud cost anomalies as they occur, to avoid unpleasant billing surprises with a FinOps approach.",
+    link: "/certifications/cloud-cost-management",
+    numberOfCerts: 3,
+  },
+  {
+    title: "Chaos Engineering Certifications",
+    module: MODULES.ce,
+    description: "Comming Soon",
+    link: "/certifications/chao-engineering",
+    numberOfCerts: 3,
+  },
 ];
