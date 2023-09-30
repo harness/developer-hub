ead807c78e444cd2c74b746acd92170df694b820
Rohan Maharjan <rohan.maharjan@harness.io>
2023-09-30 13:12:34 +0545

    HDH-183 HDH-183-investigate-tab-deep-linking

---
+++ client-modules/FocusOnAnchor.js
@@ -0,0 +1,35 @@
+import ExecutionEnvironment from "@docusaurus/ExecutionEnvironment";
+
+const FocusOnAnchor = () => {
+  const currentURL = window.location.href;
+  const url = new URL(currentURL);
+  const target = url.hash.slice(1);
+  if (url.hash) {
+    const headingsInsideDiv = document.querySelectorAll("h1, h2, h3 ,h4");
+    const changedTarget = target
+      .replace(/[^a-zA-Z0-9]/g, "")
+      .toLowerCase()
+      .replace(/\u200B/g, "")
+      .trim();
+    for (let i = 0; i < headingsInsideDiv.length; i++) {
+      const changedHeading = headingsInsideDiv[i].id
+        .replace(/[^a-zA-Z0-9]/g, "")
+        .toLowerCase()
+        .replace(/\u200B/g, "")
+        .trim();
+      if (changedHeading == changedTarget) {
+        console.log(changedHeading);
+        console.log(changedTarget);
+        console.log(headingsInsideDiv[i]);
+        headingsInsideDiv[i].scrollIntoView();
+      }
+    }
+  }
+};
+
+if (ExecutionEnvironment.canUseDOM) {
+  window.addEventListener("popstate", FocusOnAnchor);
+  window.onload = () => {
+    FocusOnAnchor();
+  };
+}
