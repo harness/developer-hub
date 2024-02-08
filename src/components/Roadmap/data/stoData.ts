43795018908be2a918fa570dd0ce3b0835efef17
priteshchandaliya <pritesh.chandaliya@harness.io>
2024-02-07 16:45:56 -0800

    Updated the STO roadmap for FY 25

--- src/components/Roadmap/data/stoData.ts
+++ src/components/Roadmap/data/stoData.ts
@@ -1,33 +1,67 @@
 import { Horizon } from "./roadmapData";
-
-export const StoData: Horizon = {
-  Now: {
-    description: "What is being delivered now",
+export const CdData: Horizon = {
+  "Now": {
+    description: "Q2 2024, May-July 2024",
     feature: [
       {
-        tag: [{ value: "K8s" }, { value: "Security" }],
-        title: "Feature Now",
-        description: "Description Now",
+        tag: [{value: "Usability"},{value: "On-boarding"}],
+        title: "Built-in Open Source Scanners",
+        description: "Simplifies onboarding open-source scanners with just a few clicks, eliminating complex configurations. We support multiple scanners in each category: SAST, SCA, Image Scanning, Secret Detection, and DAST.",
+      },
+      {
+        tag: [{value: "Integration"},{value: "Wiz"}],
+        title: "Wiz scanner support",
+        description: "Support Wiz scanner integration in all STO modes - Ingestion, orchestration, and extraction.",
+      },
+      {
+        tag: [{value: "Integration"},{value: "Semgrep"}],
+        title: "Semgrep scanner support",
+        description: "Support Commercial Semgrep scanner integration in all STO modes - Ingestion, orchestration, and extraction.",
+      },
+      {
+        tag: [{value: "Governance"},{value: "AIDA"}],
+        title: "Enhanced governance using OPA",
+        description: "Allow users to govern pipelines using out-of-the-Box OPA policies based on STO Vulnerability results.",
       },
     ],
   },
-  Next: {
-    description: "What is being developed next",
+  "Next": {
+    description: "Q3 2024, Aug-Oct 2024",
     feature: [
       {
-        tag: [{ value: "K8s" }, { value: "Security" }],
-        title: "Feature Next",
-        description: "Description Next",
+        tag: [{value: "AIDA"},{value: "Remediation"}],
+        title: "Automatic pull requests using AIDA",
+        description: "Enabling STO users to detect and directly fix issues at the source using AIDA's suggested remediation. STO applies code patches seamlessly, enhancing efficiency and ensuring a more secure codebase.",
+      },
+      {
+        tag: [{value: "EPSS"}],
+        title: "Exploit Protection Scoring System (EPSS)",
+        description: "The EPSS framework will help customers prioritize vulnerabilities faster in conjunction with other attributes such as CVSS score and severity.",
+      },
+      {
+        tag: [{value:"Reporting"}],
+        title: "Downloadable Pipeline Reports",
+        description: "Allow users to download and schedule a detailed STO results in PDF and CSV format.",
       },
     ],
   },
-  Later: {
-    description: "What is being developed later",
+  "Later": {
+    description: "Q4 2024+, Oct+ 2024",
     feature: [
       {
-        tag: [{ value: "K8s" }, { value: "Security" }],
-        title: "Feature Later",
-        description: "Description Later",
+        tag: [{value: "Dashboard"}],
+        title: "Security Workbench",
+        description: "Centralized hub for DevSecOps tasks, offering insights into organizational security posture trends, developer productivity, and comprehensive issue and exemption management across projects.",
+      },
+      {
+        tag: [{value: "Remediation"},{value: "Integration"}],
+        title: "Remediation tracker for vulnerabilities",
+        description: "Enhance the remediation workflow to seamlessly integrate with ticketing services and notification channels.",
+      },
+      {
+        tag: [{value: "IACM"},{ value: "Cross Module" }],
+        title: "STO for IACM Module",
+        description: "Extend STO support to the IACM module within Harness Platform.",
       },
     ],
   },
