807574f3e3cc668c7dd0fa7ae68e04a889601a2d
Rohan Maharjan <rohan.maharjan@harness.io>
2024-02-15 14:48:55 +0545

    HDH-182-investigate-dark-mode-to-match-harness-io

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -96,7 +96,7 @@ const config = {
         title: "Harness Developer Hub",
         logo: {
           alt: "Harness Developer Hub",
-          src: "img/logo_dlp.svg",
+          src: "/img/logo_dlp.svg",
         },
         items: [
           /*{
