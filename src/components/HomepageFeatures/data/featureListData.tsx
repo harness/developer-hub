f74cc5f86b2b0c47644537ee90c28f2b4fb9ce57
Ravi Lachhman <ravi@lachhman.io>
2024-02-19 22:25:37 -0500

    Added missing type

--- src/components/HomepageFeatures/data/featureListData.tsx
+++ src/components/HomepageFeatures/data/featureListData.tsx
@@ -9,6 +9,7 @@ export const featureList: CardItem[] = [
     icon: "img/icon_code.svg",
     description: <>Accelerate development with security at scale.</>,
     link: "docs/category/get-started-with-code",
+    type: [docType.Documentation],
   },
   {
     title: "Continuous Integration",
@@ -16,6 +17,7 @@ export const featureList: CardItem[] = [
     icon: "img/icon_ci.svg",
     description: "Build faster and be more productive.",
     link: "docs/category/get-started-with-ci",
+    type: [docType.Documentation],
   },
   {
     title: "Continuous Delivery",
@@ -24,6 +26,7 @@ export const featureList: CardItem[] = [
     description:
       "More efficient and reliable deployments.",
     link: "docs/category/get-started-with-cd",
+    type: [docType.Documentation],
   },
   {
     title: "Infrastructure as Code Management",
@@ -31,6 +34,7 @@ export const featureList: CardItem[] = [
     icon: "img/icon_iacm.svg",
     description: <>Manage your infrastructure as code, end-to-end.</>,
     link: "docs/category/get-started-with-iacm",
+    type: [docType.Documentation],
   },
   {
     title: "Feature Flags",
@@ -38,6 +42,7 @@ export const featureList: CardItem[] = [
     icon: "img/icon_ff.svg",
     description: <>Roll out new features progressively.</>,
     link: "docs/category/get-started-with-feature-flags",
+    type: [docType.Documentation],
   },
   {
     title: "Cloud Cost Management",
@@ -45,6 +50,7 @@ export const featureList: CardItem[] = [
     icon: "img/icon_ccm.svg",
     description: <>Achieve cost transparency and cut costs.</>,
     link: "docs/category/get-started-with-ccm",
+    type: [docType.Documentation],
   },
   {
     title: "Security Testing Orchestration",
@@ -52,6 +58,7 @@ export const featureList: CardItem[] = [
     icon: "img/icon_sto.svg",
     description: <>Scan code, containers, and live applications.</>,
     link: "docs/category/get-started-with-sto",
+    type: [docType.Documentation],
   },
   {
     title: "Software Supply Chain Assurance",
@@ -59,6 +66,7 @@ export const featureList: CardItem[] = [
     icon: "img/icon_ssca.svg",
     description: <>Secure your software supply chain.</>,
     link: "docs/category/get-started-with-ssca",
+    type: [docType.Documentation],
   },
   {
     title: "Service Reliability Management",
@@ -66,6 +74,7 @@ export const featureList: CardItem[] = [
     icon: "img/icon_srm.svg",
     description: <>Monitor SLOs, track error budgets, and analyze impact of changes.</>,
     link: "docs/category/get-started-with-srm",
+    type: [docType.Documentation],
   },
   {
     title: "Chaos Engineering",
@@ -73,6 +82,7 @@ export const featureList: CardItem[] = [
     icon: "img/icon_ce.svg",
     description: <>Ensure application and infrastructure resilience.</>,
     link: "docs/category/get-started-with-ce",
+    type: [docType.Documentation],
   },
   {
     title: "Continuous Error Tracking",
@@ -82,6 +92,7 @@ export const featureList: CardItem[] = [
       <>Identify, triage, and resolve errors proactively.</>
     ),
     link: "docs/category/get-started-with-cet",
+    type: [docType.Documentation],
   },
   {
     title: "Internal Developer Portal",
@@ -89,6 +100,7 @@ export const featureList: CardItem[] = [
     icon: "img/icon_idp.svg",
     description: <>A home for developers to create, manage, and explore software.</>,
     link: "docs/category/get-started-with-idp",
+    type: [docType.Documentation],
   },
   {
     title: "Software Engineering Insights",
@@ -96,6 +108,7 @@ export const featureList: CardItem[] = [
     icon: "img/icon_sei.svg",
     description: <>Use data-led insights to remove bottlenecks and improve productivity.</>,
     link: "docs/category/get-started-with-sei",
+    type: [docType.Documentation],
   },
   {
     title: "Harness Platform",
@@ -103,6 +116,7 @@ export const featureList: CardItem[] = [
     icon: "img/logo.svg",
     description: <>Deliver software with the highest velocity, quality, security, reliability, and resilience.</>,
     link: "docs/category/get-started-with-platform",
+    type: [docType.Documentation],
   },
   {
     title: "Harness Self-Managed Enterprise Edition",
@@ -110,5 +124,6 @@ export const featureList: CardItem[] = [
     icon: "img/logo.svg",
     description: <>Install Harness Platform on self-managed Kubernetes cluster.</>,
     link: "docs/category/get-started-with-smp",
+    type: [docType.Documentation],
   },
 ];
