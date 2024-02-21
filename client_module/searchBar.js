84d6d247f7f1937f466f7b2d720f270b2f421411
Rohan Maharjan <rohan.maharjan@harness.io>
2024-02-21 11:45:16 +0545

    HDH 226 click on search navigates to main menu

--- client_module/searchBar.js
+++ client_module/searchBar.js
@@ -12,6 +12,21 @@ const doYourCustomStuff = () => {
 
   if (navbar__items) {
     navbar__items.appendChild(searchButton);
+    if (window.location.pathname !== "/") {
+      const navbar__toggle = document.querySelectorAll(".navbar__toggle");
+      if (navbar__toggle) {
+        navbar__toggle[0].addEventListener("click", () => {
+          const navbar__sidebar__items = document.querySelectorAll(
+            ".navbar-sidebar__items"
+          );
+          if (navbar__sidebar__items[0]) {
+            navbar__sidebar__items[0].classList.add(
+              "navbar-sidebar__items--show-secondary"
+            );
+          }
+        });
+      }
+    }
 
     searchButton.addEventListener("click", () => {
       const navbar = document.querySelector(".navbar");
