526955376ded672818d1d13a5c3057304f6066f2
SmritiSatya <smriti.satyanarayana@harness.io>
2024-02-29 14:09:18 +0530

    template for srmData

--- src/components/Roadmap/data/srmData.ts
+++ src/components/Roadmap/data/srmData.ts
@@ -5,19 +5,39 @@ export const SrmData: Horizon = {
     description: "Q1 2024, Feb-Apr 2024",
     feature: [
       {
-        tag: [{ value: "K8s" }, { value: "Security" }],
-        title: "Feature Now",
-        description: "Description Now",
+        tag: [{ value: "Reliability platform" }],
+        title: "",
+        description: "",
+      },
+      {
+        tag: [{ value: "Reliability platform" }],
+        title: "",
+        description: "",
+      },
+      {
+        tag: [{ value: "Reliability integrations" }],
+        title: "",
+        description: "",
       },
     ],
   },
   Next: {
     description: "Q2 2024, May-Jul 2024",
     feature: [
+{
+        tag: [{ value: "Reliability platform" }],
+        title: "",
+        description: "",
+      },
       {
-        tag: [{ value: "K8s" }, { value: "Security" }],
-        title: "Feature Next",
-        description: "Description Next",
+        tag: [{ value: "Reliability platform" }],
+        title: "",
+        description: "",
+      },
+      {
+        tag: [{ value: "Reliability integrations" }],
+        title: "",
+        description: "",
       },
     ],
   },
@@ -25,9 +45,39 @@ export const SrmData: Horizon = {
     description: "Q3 2024+, Aug 2024 & beyond",
     feature: [
       {
-        tag: [{ value: "K8s" }, { value: "Security" }],
-        title: "Feature Later",
-        description: "Description Later",
+        tag: [{ value: "Reliability platform" }],
+        title: "",
+        description: "",
+      },
+      {
+        tag: [{ value: "Reliability platform" }],
+        title: "",
+        description: "",
+      },
+      {
+        tag: [{ value: "Reliability integrations" }],
+        title: "",
+        description: "",
+      },
+    ],
+  },
+  Released: {
+    description: "What has been released",
+    feature: [
+      {
+        tag: [{ value: "Reliability platform" }],
+        title: "",
+        description: "",
+      },
+      {
+        tag: [{ value: "Reliability platform" }],
+        title: "",
+        description: "",
+      },
+      {
+        tag: [{ value: "Reliability integrations" }],
+        title: "",
+        description: "",
       },
     ],
   },
