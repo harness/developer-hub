f32b961cbf5bc77aabc25557de814e71d9d7676b
richardblack-Harness <richard.black@harness.io>
2024-08-02 09:14:29 +0100

    IAC-2279 Update tile headings

--- src/components/Docs/data/iacmData.ts
+++ src/components/Docs/data/iacmData.ts
@@ -10,13 +10,13 @@ export const docsCards: CardSections = [
     description: "",
     list: [
       {
-        title: "IaCM Onboarding",
+        title: "Onboarding",
         module: MODULES.iacm,
         description: "Get onboarded with Harness Infrastructure as Code Management.",
         link: "/docs/infra-as-code-management/get-started/onboarding-guide",
       },
       {
-        title: "IaCM Overview",
+        title: "Overview",
         module: MODULES.iacm,
         description: "Learn about Harness Infrastructure as Code Management.",
         link: "/docs/infra-as-code-management/get-started/overview",
