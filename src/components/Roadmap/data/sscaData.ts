d8d5cf1df3e47daf67fd4be773a1104af80e34b5
Pranay Shah <89774391+pranay-harness@users.noreply.github.com>
2024-08-12 12:35:59 +0530

    Update sscaData.ts

--- src/components/Roadmap/data/sscaData.ts
+++ src/components/Roadmap/data/sscaData.ts
@@ -6,13 +6,8 @@ export const SscaData: Horizon = {
     feature: [
       {
         tag: [],
-        title: "Repo Security Posture Management",
-        description: "Configuration checks to ensure that your source code repositories used in the supply chain are appropriately monitored for best security practices, including SBOM generation and security tests such as SAST, SCA, and secrets scanning.",
-      },
-      {
-        tag: [],
-        title: "Support for GitHub",
-        description: "Complete support for GitHub, allowing users to onboard using the GitHub app and perform configuration checks, SBOM generation, and security scans.",
+        title: "Repo Security Posture Management for GitHub",
+        description: "Identify misconfigs in source code repositories based on industry standards such as CIS and OWASP Top 10 CI/CD Security Risks. Also, includes support for SBOM generation and security tests such as SAST, SCA, and secrets scanning.",
       },
       {
         tag: [],
@@ -42,17 +37,17 @@ export const SscaData: Horizon = {
       {
         tag: [],
         title: "Report Generation",
-        description: "Generate and download reports based on compliance standards such as CIS, OWASP, and more.",
+        description: "Generate and download reports based on compliance standards such as CIS, and OWASP Top 10 CI/CD Security Risks",
       },
       {
         tag: [],
-        title: "CI/CD Security Posture Management",
-        description: "Checks on CI/CD pipelines to detect risky actions and misconfigs",
+        title: "CI/CD Security Posture Management for GitHub Workflows & Harness Pipelines:",
+        description: "Perform static analysis in Github workflows and Harness pipelines to detect risky actions and misconfigurations. ",
       },
       {
         tag: [],
-        title: "Support for Harness Code for RSPM",
-        description: "Complete support for Harness Code, allowing users to onboard using the GitHub app and perform configuration checks, SBOM generation, and security scans.",
+        title: "Repo Security Posture Management for Harness Code",
+        description: "Identify misconfigs in source code repositories based on industry standards such as CIS and OWASP Top 10 CI/CD Risk. Also, includes support for SBOM generation and security tests such as SAST, SCA, and secrets scanning.",
       },
       {
         tag: [],
@@ -61,8 +56,8 @@ export const SscaData: Horizon = {
       },
       {
         tag: [],
-        title: "Vault Support for SBOM & SLSA attestation and verification",
-        description: "Cosign interacts with Vault's transient engine to directly handle SBOM and SLSA attestation and verification, eliminating the need for storing secrets with Harness.",
+        title: "HashiCorp Vault Support",
+        description: "Leverage keys from HashiCorp Vault to attest and verify your artifacts.",
       },
     ],
   },
@@ -71,18 +66,18 @@ export const SscaData: Horizon = {
     feature: [
       {
         tag: [],
-        title: "OpenSSF, OWASP Top 10 OSS, EO14028 Support",
-        description: "Added support for OpenSSF, OWASP Top 10 OSS, and EO14028 compliance standards.",
+        title: "OpenSSF, OWASP Top 10 OSS, EO14028 & NIST SP800-204D Support",
+        description: "Out of the box rules for supporting OpenSSF, OWASP Top 10 OSS, EO14028, & NIST SP800-204D compliance standards.",
       },
       {
         tag: [],
-        title: "Remediation Tracker Support for Compliance Standards Violations",
-        description: "Create and manage remediation trackers for compliance standard violations.",
+        title: "Remediation Tracker Support for Compliance Standards Issues",
+        description: "Assign issues to developers and track them with a remediation tracker to ensure governance on compliance standards.",
       },
       {
         tag: [],
-        title: "Governance Policies for Compliance Standard",
-        description: "Allow users to enforce governance policies against the supported compliance standards.",
+        title: "Governance Policies for Compliance Standards",
+        description: "View and enforce policies on Code Repo, Artifacts and CI/CD pipelines based on rules defined in supply chain standards.",
       },
     ],
   },
@@ -91,28 +86,23 @@ export const SscaData: Horizon = {
     feature: [
       {
         tag: [],
-        title: "Support for Gitlab",
-        description: "Complete support for Gitlab, allowing users to onboard gitlab repositories and perform configuration checks, SBOM generation, and security scans.",
+        title: "Support for Gitlab & CircleCI",
+        description: "Complete support for GitLab, allowing users to onboard GitLab repositories and perform configuration checks, SBOM generation, and security scans.",
       },
       {
         tag: [],
-        title: "Support for CircleCI",
-        description: "Onboard CircleCI pipelines to check for Risk & Compliance Issues and misconfigurations.",
+        title: "CI/CD Security for Jenkins",
+        description: "Perform static analysis in Jenkins pipelines to detect risky plugins and misconfigurations.",
       },
       {
         tag: [],
-        title: "SBOM Risk Scoring",
-        description: "Added support for SBOM scoring against risky OSS components.",
+        title: "Harness Runtime Analyzer",
+        description: "Identify anomalous behavior and threats in Harness Pipelines and GitHub Workflows",
       },
       {
         tag: [],
-        title: "OpenSSF Scorecard Integration",
-        description: "Assess open source projects for security risks through a series of automated checks.",
-      },
-      {
-        tag: [],
-        title: "NIST SP800-204D Support",
-        description: "Added support for NIST SP800-204D compliance standard.",
+        title: "SBOM Scoring in Drift Detection",
+        description: "View risk scores on dependencies that get added or removed between artifact drifts which contain vulnerabilities, have invalid licenses or are unmaintained.",
       },
     ],
   },
