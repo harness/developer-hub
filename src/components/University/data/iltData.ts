dfbc102652a3e2d7bad2f7284c5ac3f3dbeecbe3
Ravi Lachhman <ravi@lachhman.io>
2024-10-24 11:19:53 -0400

    GA of IDP ILT

--- src/components/University/data/iltData.ts
+++ src/components/University/data/iltData.ts
@@ -79,4 +79,15 @@ export const ilt: IltCardItem[] = [
     tileType: tileType.normal,
     cardType: cardType.ILT,
   },
+  {
+    title: "Internal Developer Portal",
+    module: MODULES.idp,
+    type: type.user,
+    description:
+      "Deep dive into advanced IDP and developer self-service concepts.",
+    version: "Harness IDP Enterprise Plan",
+    link: "https://university-registration.harness.io/ilt-internal-developer-portal",
+    tileType: tileType.normal,
+    cardType: cardType.ILT,
+  },
 ];
