eb902a0090497758cf6657b285c4d43632f3e8f9
Dave Karow <david.karow@harness.io>
2024-11-11 13:22:07 -0800

    Update fmeData.ts to reflect Now, Next, Later as of Nov 2024

--- src/components/Roadmap/data/fmeData.ts
+++ src/components/Roadmap/data/fmeData.ts
@@ -2,13 +2,8 @@ import { Horizon } from "./roadmapData";
 
 export const FmeData: Horizon = {
   Now: {
-    description: "Q3 2024, Aug-Oct 2024",
+    description: "Q4 2024, Nov 2024-Jan 2025",
     feature: [
-      {
-        tag: [{ value: "Measurement" }],
-        title: "Rum agents sampling",
-        description: "Control the number of events that are auto-captured by the Split Suite and RUM agents.",
-      },
       {
         tag: [{ value: "Targeting" }],
         title: "Large segments",
@@ -19,11 +14,43 @@ export const FmeData: Horizon = {
         title: "Access Split from within Harness app",
         description: "Allow Harness customers to authenticate and access Split from the Harness application.",
       },
+      {
+        tag: [{ value: "Targeting" }],
+        title: "Flag impressions toggle",
+        description: "Disable the flow of impressions for individual flags.",
+      },
+      {
+        tag: [{ value: "Monitoring" }],
+        title: "Feature flag alerts on Monitoring tab",
+        description: "Show significance alerts on the monitoring tab, in addition to the threshold-based alerts shown now.",
+      },
+      {
+        tag: [{ value: "Measurement" }],
+        title: "Rum agents sampling",
+        description: "Control the number of events that are auto-captured by the Split Suite and RUM agents.",
+      },
+      {
+        tag: [{ value: "Targeting" }],
+        title: "SemVer attribute dictionary support",
+        description: "Support SemVer type attributes and suggested values in the attribute dictionary to streamline entry & reduce risk of errors.",
+      },
     ],
   },
   Next: {
-    description: "Q4 2024, Nov 2024-Jan 2025",
+    description: "Q1 2025, Feb-Apr 2025",
     feature: [
+      {
+        tag: [{ value: "Targeting" }],
+        title: "Flag Prerequisites",
+        description:
+          "Top level flag dependencies enforced (new) as well as within rules (existing).",
+      },
+      {
+        tag: [{value: "Targeting"}, {value: "Better Together"}],
+        title: "Rule-based segments",
+        description:
+          "Assign feature availability for user groups based on different conditions, with all the power of Split targeting.",
+      },
       {
         tag: [{ value: "Better Together" }],
         title: "Split Integration into Harness",
@@ -36,16 +63,10 @@ export const FmeData: Horizon = {
         description:
           "A new, refreshed view for experimentation results to help simplify data interpretation.",
       },
-      {
-        tag: [{ value: "Targeting" }],
-        title: "Flag Prerequisites",
-        description:
-          "Top level flag dependencies enforced (new) as well as within rules (existing).",
-      },
     ],
   },
   Later: {
-    description: "Q1 2025+, Feb 2025 & beyond",
+    description: "Q2 2025+, May 2025 & beyond",
     feature: [
       {
         tag: [{ value: "Better Together" }],
@@ -61,11 +82,6 @@ export const FmeData: Horizon = {
         tag: [{ value: "SDK" }],
         title: "Extend SDK support",
         description: "Support additional SDKs like Erlang.",
-      },
-      {
-        tag: [{value: "Targeting"}, {value: "Better Together"}],
-        title: "Rule-based segments",
-        description: "Assign feature availability for user groups based on different conditions, with all the power of Split targeting.",
       },
     ],
   },
@@ -89,13 +105,13 @@ export const FmeData: Horizon = {
       },
       {
         tag: [{ value: "Measurement" }, {value: "SDK"}],
-        title: "Split Suite for iOS",
+        title: "Split Suite for Browser, iOS, Android",
         description: "Eliminate the need to connect the RUM agents to the SDK separately with a single unified SDK.",
       },
       {
         tag: [{ value: "Measurement" }, {value: "Experimentation"}],
-        title: "Guardrail metrics",
-        description: "A new category of metrics that can be used to protect every release and improve metric organization.",
+        title: "Guardrail metrics and alerts",
+        description: "A new category of metrics and alerts that can be used to protect every release and improve metric organization.",
       },
       {
         tag: [{ value: "SDK" }, {value: "Security"}],
