74c52c1b3d4cadcd7196a063ce7681ca16009ff0
SmritiSatya <117705907+SmritiSatya@users.noreply.github.com>
2024-12-06 14:47:28 +0530

    Pod JVM trigger gc
    Pod JVM trigger gc docs and fault diagram

--- docs/chaos-engineering/use-harness-ce/chaos-faults/kubernetes/experiments.ts
+++ docs/chaos-engineering/use-harness-ce/chaos-faults/kubernetes/experiments.ts
@@ -200,6 +200,12 @@ export const experiments: ExperimentDetails[] = [
     category: "kubernetes",
   },
   {
+    name: "Pod JVM trigger gc",
+    description:"Pod JVM trigger gc triggers the garbage collector for a Java process executing in a Kubernetes pod. This causes unused (or out of scope) objects and variables to be garbage collected and recycled, thereby freeing up memory space.",
+    tags: ["pod", "jvm"],
+    category: "kubernetes",
+  },
+  {
     name: "Pod memory hog exec",
     description:"Pod memory hog exec consumes memory resources on the application container in megabytes.",
     tags: ["pod"],
