d0b034904abf3bb3087b8be2d9c35e57d3640988
Rohan Maharjan <rohan.maharjan@harness.io>
2025-01-26 20:05:51 +0545

    chore:typescript update

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,41 +1,48 @@
-import React from "react";
-import clsx from "clsx";
-import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
-import Layout from "@theme/Layout";
-import HomepageFeatures from "@site/src/components/HomepageFeatures";
-import LearnAboutPlatform from "@site/src/components/LearnAboutPlatform";
-import HomepageUniversity from "@site/src/components/HomepageUniversity";
-import Feedback from "@site/src/components/Feedback";
-import MDXContent from "@theme/MDXContent";
-import Lottie from "lottie-react";
-import allModuleAnimation from "./assets/hdh_hero.json";
-import allModuleAnimationDark from "./assets/hdh_hero-dark.json";
+import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
+import Layout from '@theme/Layout';
+import MDXContent from '@theme/MDXContent';
+import BrowserOnly from '@docusaurus/BrowserOnly';
+import clsx from 'clsx';
+import React from 'react';
+import allModuleAnimationDark from './assets/hdh_hero-dark.json';
+import allModuleAnimation from './assets/hdh_hero.json';
+import HomepageFeatures from '@site/src/components/HomepageFeatures';
+import LearnAboutPlatform from '@site/src/components/LearnAboutPlatform';
+import HomepageUniversity from '@site/src/components/HomepageUniversity';
+import Feedback from '@site/src/components/Feedback';
+import styles from './index.module.scss';
 
-import styles from "./index.module.scss";
-
-import { useColorMode } from "@docusaurus/theme-common";
-import Chatbot from "../components/Chatbot/Chatbot";
+import { useColorMode } from '@docusaurus/theme-common';
 function HomepageHeader() {
   const { colorMode } = useColorMode();
   const { siteConfig } = useDocusaurusContext();
   return (
-    <div style={{ position: "relative" }}>
-      <header className={clsx("container", styles.heroBanner)}>
-        <div className={styles.heroContainer}>
-          <h1 className={styles.heroTitle}>{siteConfig.title}</h1>
-          <p className={styles.heroSubTitle}>{siteConfig.tagline}</p>
-        </div>
-      </header>
-      <div className={styles.heroImg}>
-        <Lottie
-          animationData={
-            colorMode === "dark" ? allModuleAnimationDark : allModuleAnimation
-          }
-          loop={true}
-        />
-        {/* <Lottie animationData={allModuleAnimationDark} loop={true} /> */}
-      </div>
-    </div>
+    <BrowserOnly fallback={<div>Loading...</div>}>
+      {() => {
+        const Lottie = require('lottie-react').default;
+
+        return (
+          <div style={{ position: 'relative' }}>
+            <header className={clsx('container', styles.heroBanner)}>
+              <div className={styles.heroContainer}>
+                <h1 className={styles.heroTitle}>{siteConfig.title}</h1>
+                <p className={styles.heroSubTitle}>{siteConfig.tagline}</p>
+              </div>
+            </header>
+            <div className={styles.heroImg}>
+              <Lottie
+                animationData={
+                  colorMode === 'dark'
+                    ? allModuleAnimationDark
+                    : allModuleAnimation
+                }
+                loop={true}
+              />
+            </div>
+          </div>
+        );
+      }}
+    </BrowserOnly>
   );
 }
 
