1dddefb44c78b8106da401076d9cb4703fd08813
richardblack-Harness <richard.black@harness.io>
2025-01-27 10:45:31 +0000

    IAC-3015 Update landing page url to test iacm

--- src/components/HomepageFeatures/data/featureListData.tsx
+++ src/components/HomepageFeatures/data/featureListData.tsx
@@ -44,7 +44,7 @@ export const featureList: CardItem[] = [
     module: MODULES.iacm,
     icon: "img/icon_iacm.svg",
     description: <>Manage your infrastructure with ease.</>,
-    link: "docs/infra-as-code-management/get-started/onboarding-guide",
+    link: "docs/category/get-started-with-iacm",
   },
   {
     title: "Release Faster with Feature Flags",
