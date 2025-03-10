ba3e7730b878b65b25415219decd9a6cdbbcc414
joeyouss <jyoti.bisht@harness.io>
2025-03-11 03:39:18 +0530

    fixed broken links

--- src/components/Docs/data/cloudCostManagementData.ts
+++ src/components/Docs/data/cloudCostManagementData.ts
@@ -87,7 +87,7 @@ import { MODULES } from "@site/src/constants";
             module: MODULES.ccm,
             description:
               "Identify unusual or unexpected changes in your cloud service expenses.",
-            link: "/docs/cloud-cost-management/use-ccm-cost-reporting/anomaly-detection",
+            link: "/docs/cloud-cost-management/use-ccm-cost-reporting/anomaly-detection/a-detect-cloud-cost-anomalies-with-ccm",
           },
           {
             title: "CCM FAQs",
