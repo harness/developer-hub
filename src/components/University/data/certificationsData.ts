cb2ed016787ff72324d1da86be34f54f851a8b9e
Ravi Lachhman <ravi@lachhman.io>
2025-07-02 16:21:52 -0400

    Updated order of Harness Univ Items

--- src/components/University/data/certificationsData.ts
+++ src/components/University/data/certificationsData.ts
@@ -3,34 +3,14 @@ import { MODULES } from "../../../constants";
 
 export const university: CardItem[] = [
   {
-    title: "Continuous Delivery & GitOps - Developer",
-    module: MODULES.cd,
+    title: "Code Repository  - Developer",
+    module: MODULES.code,
     type: certType.developer,
     description:
-      "CD & GitOps Developer focuses on the fundamental skills to deploy your applications with CD & GitOps projects.",
-    version: "Harness CD & GitOps Free Plans",
-    link: "/university/continuous-delivery?lvl=developer",
-    // numberOfCerts: 1,
-  },
-  {
-    title: "Continuous Delivery & GitOps - Administrator",
-    module: MODULES.cd,
-    type: certType.administrator,
-    description:
-      "CD & GitOps Administrator focuses the fundamental skills to deploy and maintain CD & GitOps projects and the overall Harness Platform. This exam builds upon the CD & GitOps Developer Certification.",
-    version: "Harness CD & GitOps Paid Plans",
-    link: "/university/continuous-delivery?lvl=administrator",
-    // numberOfCerts: 1,
-  },
-  {
-    title: "Continuous Delivery & GitOps - Architect",
-    module: MODULES.cd,
-    type: certType.architect,
-    description:
-      "CD & GitOps Architect focuses on key technical job functions and advanced skills in design, implementation and management of CD & GitOps. This exam builds upon the CD & GitOps Administrator Certification.",
-    version: "Harness CD & GitOps Paid Plans",
-    link: "/university/continuous-delivery?lvl=architect",
-    // numberOfCerts: 1,
+      "CR Developer focuses on the fundamental skills around SCM/Git and governance.",
+    version: "Harness Paid Plans",
+    link: "/university/cr?lvl=developer",
+    // numberOfCerts:  1, 
   },
   {
     title: "Continuous Integration - Developer",
@@ -63,6 +43,47 @@ export const university: CardItem[] = [
     // numberOfCerts: 1,
   },
   {
+    title: "Continuous Delivery & GitOps - Developer",
+    module: MODULES.cd,
+    type: certType.developer,
+    description:
+      "CD & GitOps Developer focuses on the fundamental skills to deploy your applications with CD & GitOps projects.",
+    version: "Harness CD & GitOps Free Plans",
+    link: "/university/continuous-delivery?lvl=developer",
+    // numberOfCerts: 1,
+  },
+  {
+    title: "Continuous Delivery & GitOps - Administrator",
+    module: MODULES.cd,
+    type: certType.administrator,
+    description:
+      "CD & GitOps Administrator focuses the fundamental skills to deploy and maintain CD & GitOps projects and the overall Harness Platform. This exam builds upon the CD & GitOps Developer Certification.",
+    version: "Harness CD & GitOps Paid Plans",
+    link: "/university/continuous-delivery?lvl=administrator",
+    // numberOfCerts: 1,
+  },
+  {
+    title: "Continuous Delivery & GitOps - Architect",
+    module: MODULES.cd,
+    type: certType.architect,
+    description:
+      "CD & GitOps Architect focuses on key technical job functions and advanced skills in design, implementation and management of CD & GitOps. This exam builds upon the CD & GitOps Administrator Certification.",
+    version: "Harness CD & GitOps Paid Plans",
+    link: "/university/continuous-delivery?lvl=architect",
+    // numberOfCerts: 1,
+  },
+  {
+    title: "Infrastructure as Code Management  - Developer",
+    module: MODULES.iacm,
+    type: certType.developer,
+    description:
+      "IaCM Developer focuses on the fundamental skills to scale your Terraform / OpenTofu Infrastructure as Code.",
+    version: "Harness IaCM Free Plans",
+    link: "/university/iacm?lvl=developer",
+    // numberOfCerts: 1,
+  },
+
+  {
     title: "Feature Flags - Developer",
     module: MODULES.ff,
     type: certType.developer,
@@ -70,6 +91,16 @@ export const university: CardItem[] = [
       "FF Developer focuses on the fundamental skills to progressivly deploy/toggle your applications with FF projects.",
     version: "Harness FF Free Plans",
     link: "/university/feature-flags?lvl=developer",
+    // numberOfCerts: 1,
+  },
+  {
+    title: "Feature Flags - Administrator",
+    module: MODULES.ff,
+    type: certType.administrator,
+    description:
+      "Feature Flag Administrator focuses the fundamental skills to deploy, maintain, and customize feature flags in the Harness platform. This exam builds upon the FF Developer Certification.",
+    version: "Harness FF Paid Plans",
+    link: "/university/feature-flags?lvl=administrator",
     // numberOfCerts: 1,
   },
   {
@@ -83,13 +114,13 @@ export const university: CardItem[] = [
     // numberOfCerts: 1,
   },
   {
-    title: "Feature Flags - Administrator",
-    module: MODULES.ff,
+    title: "Cloud Cost Management  - Administrator",
+    module: MODULES.ccm,
     type: certType.administrator,
     description:
-      "Feature Flag Administrator focuses the fundamental skills to deploy, maintain, and customize feature flags in the Harness platform. This exam builds upon the FF Developer Certification.",
-    version: "Harness FF Paid Plans",
-    link: "/university/feature-flags?lvl=administrator",
+      "CCM Administrator focuses on the advanced skills in design, implementation, and management of cloud cost savings.",
+    version: "Harness CCM Paid Plans",
+    link: "/university/cloud-cost-management?lvl=administrator",
     // numberOfCerts: 1,
   },
   {
@@ -113,13 +144,13 @@ export const university: CardItem[] = [
     // numberOfCerts: 1,
   },
   {
-    title: "Cloud Cost Management  - Administrator",
-    module: MODULES.ccm,
-    type: certType.administrator,
+    title: "Supply Chain Security  - Developer",
+    module: MODULES.ssca,
+    type: certType.developer,
     description:
-      "CCM Administrator focuses on the advanced skills in design, implementation, and management of cloud cost savings.",
-    version: "Harness CCM Paid Plans",
-    link: "/university/cloud-cost-management?lvl=administrator",
+      "SCS Developer focuses on the fundamental skills to secure your SDLC with risk frameworks.",
+    version: "Harness SCS Paid Plans",
+    link: "/university/scs?lvl=developer",
     // numberOfCerts: 1,
   },
   {
@@ -130,6 +161,16 @@ export const university: CardItem[] = [
       "Chaos Developer focuses on the fundamental skills to inject failure into your applications to build resilience.",
     version: "Harness Chaos Free Plans",
     link: "/university/chaos-engineering?lvl=developer",
+    // numberOfCerts: 1,
+  },
+  {
+    title: "Chaos Engineering  - Administrator",
+    module: MODULES.ce,
+    type: certType.administrator,
+    description:
+      "Chaos Administrator focuses focuses on the advanced skills in of injecting failure into your applications to build resilience.",
+    version: "Harness Chaos Paid Plans",
+    link: "/university/chaos-engineering?lvl=administrator",
     // numberOfCerts: 1,
   },
   {
@@ -152,44 +193,5 @@ export const university: CardItem[] = [
     link: "/university/sei?lvl=developer",
     // numberOfCerts: 1,
   },
-  {
-    title: "Infrastructure as Code Management  - Developer",
-    module: MODULES.iacm,
-    type: certType.developer,
-    description:
-      "IaCM Developer focuses on the fundamental skills to scale your Terraform / OpenTofu Infrastructure as Code.",
-    version: "Harness IaCM Free Plans",
-    link: "/university/iacm?lvl=developer",
-    // numberOfCerts: 1,
-  },
-  {
-    title: "Chaos Engineering  - Administrator",
-    module: MODULES.ce,
-    type: certType.administrator,
-    description:
-      "Chaos Administrator focuses focuses on the advanced skills in of injecting failure into your applications to build resilience.",
-    version: "Harness Chaos Paid Plans",
-    link: "/university/chaos-engineering?lvl=administrator",
-    // numberOfCerts: 1,
-  },
-  {
-    title: "Supply Chain Security  - Developer",
-    module: MODULES.ssca,
-    type: certType.developer,
-    description:
-      "SCS Developer focuses on the fundamental skills to secure your SDLC with risk frameworks.",
-    version: "Harness SCS Paid Plans",
-    link: "/university/scs?lvl=developer",
-    // numberOfCerts: 1,
-  },
-  {
-    title: "Code Repository  - Developer",
-    module: MODULES.code,
-    type: certType.developer,
-    description:
-      "CR Developer focuses on the fundamental skills around SCM/Git and governance.",
-    version: "Harness Paid Plans",
-    link: "/university/cr?lvl=developer",
-    // numberOfCerts:  1, 
-  },
+
 ];
