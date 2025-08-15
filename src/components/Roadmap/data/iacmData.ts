fe4d4a2f4b4a50be82ca75ff2d22652b8ad11e2a
richardblack-Harness <richard.black@harness.io>
2025-08-15 10:21:48 +0100

    IAC-4340 IaCM Roadmap update

--- src/components/Roadmap/data/iacmData.ts
+++ src/components/Roadmap/data/iacmData.ts
@@ -6,8 +6,20 @@ export const IacmData: Horizon = {
     feature: [
       {
         tag: [{ value: "Developer Experience" }],
-        title: "Looker Dashboard",
-        description: "IaCM provides a Looker dashboard that allows you to monitor the state of your resources and track the cost of your resources",
+        title: "Module Registry Testing",
+        description: "Automate the validation and testing of modules in isolated environments before publishing, ensuring only high-quality, compliant modules are available in the private Module Registry.",
+        link: "/docs/infra-as-code-management/iacm-features/module-registry/module-registry-testing",
+      },
+      {
+        tag: [{ value: "Simplify Onboarding" }],
+        title: "Workspace templates",
+        description: "Users will be able to create Workspaces that will inherit cost, OPA, and other configurations from templates, simplifying onboarding and enforcing best practices",
+        link: "/docs/infra-as-code-management/workspaces/workspace-tempates",
+      },  
+      {
+        tag: [{ value: "Reports & Insights" }],
+        title: "BI Dashboard",
+        description: "Users can now create custom BI Dashboards to track their compliance, usage trends with alerting and reporting.",
         link: "/docs/infra-as-code-management/reports-insights/dashboards",
       },
       {
@@ -28,7 +40,7 @@ export const IacmData: Horizon = {
         link: "/docs/infra-as-code-management/policies-governance/opa-workspace",
       },
       {
-        tag: [],
+        tag: [{ value: "Reports & Insights" }],
         title: "Cost estimation",
         description: "Using Infracost, IaCM provides cost estimation on resource changes, allowing you to understand how your cloud bill will be changed, based on resource changes",
         link: "/docs/infra-as-code-management/workspaces/cost-estimation",
@@ -81,49 +93,52 @@ export const IacmData: Horizon = {
     ],
   },
   Now: {
-    description: "Q2 2025, May 2025-July 2025",
+    description: "Q2 2025, August 2025-October 2025",
     feature: [
       {
         tag: [{ value: "Developer Experience" }],
-        title: "Module Registry Testing",
-        description: "Automate the validation and testing of modules in isolated environments before publishing, ensuring only high-quality, compliant modules are available in the private Module Registry.",
-        link: "/docs/infra-as-code-management/iacm-features/module-registry/module-registry-testing",
+        title: "Ansible Support (Beta)",
+        description: "Configuration management support with native playbooks, static and dynamic inventory",
       },
       {
-        tag: [{ value: "Simplify Onboarding" }],
-        title: "Workspace templates",
-        description: "Users will be able to create Workspaces that will inherit cost, OPA, and other configurations from templates, simplifying onboarding and enforcing best practices",
-        link: "/docs/infra-as-code-management/workspaces/workspace-tempates",
+        tag: [{ value: "Developer Experience" }],
+        title: "Private Provider Registry (Beta)",
+        description: "Native support to store and manage private providers",
       },
-      {    
+      {
+        tag: [{ value: "Developer Experience" }],
+        title: "Terragrunt Support (Beta)",
+        description: "Native Terragrunt support for IaCM Workspaces",
+      },
+      {
         tag: [{ value: "Developer Experience" }],
         title: "Variable Sets",
-        description: "Define reusable collections of OpenTofu/Terraform and environment variables, as well as connectors, that can be used across different workspaces. This feature makes it easy to manage variables centrally while maintaining security for sensitive data.",
-      },
-      {
-        tag: [{ value: "Developer Experience" }],
-        title: "Provider Registry",
-        description: "Centralized provider repository where users can discover, download, and use providers that enable Terraform to interact with APIs and services",
+        description: "Support for variable sets to streamline management and improve reusability",
       },
     ],
   },
   Next: {
-    description: "Q3 2025, Aug 2025-Oct 2025",
+    description: "Q3 2025, November 2025-January 2026",
     feature: [
       {
-        tag: [],
-        title: "Terragrunt support",
-        description: "Supporting Terragrunt as an IaC provider",
+        tag: [{ value: "Developer Experience" }],
+        title: "Ansible Support (GA)",
+        description: "Native Ansible approvals, Ansible galaxy support, BI Dashboards.",
       },
       {
-        tag: [],
-        title: "Native Ansible Support",
-        description: "Supporting Ansible as an IaC provider",
+        tag: [{ value: "Developer Experience" }],
+        title: "Terragrunt Support (GA)",
+        description: "Native Terragrunt Approvals, OPA, Cost estimation.",
       },
       {
         tag: [{ value: "Developer Experience" }],
         title: "Ephemeral environments",
-        description: "Users will be able to control the TTL configuration of each managed Workspace",
+        description: "Users will be able to control the TTL configuration of each managed Workspace.",
+      },
+      {
+        tag: [{ value: "Developer Experience" }],
+        title: "Harness CCM Integration",
+        description: "Integration with CCM allows IaCM users to have full visibility of costs associated with the resources being managed by IaCM",
       },
     ],
   },
@@ -131,39 +146,24 @@ export const IacmData: Horizon = {
     description: "Q4 2025+ & beyond",
     feature: [
       {
-        tag: [],
-        title: "Harness CCM Integration",
-        description: "Integration with CCM allows IaCM users to have full visibility of costs associated with the resources being managed by IaCM",
-      },
-      {
-        tag: [],
-        title: "Infrastructure AI Agent",
+        tag: [{ value: "AI" }],
+        title: "AI Infrastructure Agent",
         description: "An agentic AI experience that proactively accelerates and automates infrastructure management and optimization with minimal human intervention.",
       },
       {
-        tag: [],
+        tag: [{ value: "Developer Experience" }],
         title: "Harness CD integration",
         description: "Integration with CD allows IaCM users to link Resources with Services and Environments, giving end-to-end visibility",
       },
       {
         tag: [{ value: "Developer Experience" }],
-        title: "Debugger",
-        description: "Debbuger will simplify the troubleshooting process for execution failures",
+        title: "AWS CloudFormation support",
+        description: "Supporting AWS CloudFormation as an IaC provider",
       },
       {
         tag: [{ value: "Developer Experience" }],
-        title: "Cache intelligence",
-        description: "This feature will speed up execution time by caching libraries and containers that are frequently used",
-      },
-      {
-        tag: [],
         title: "CDK Support",
         description: "Supporting CDK as an IaC provider",
-      },
-      {
-        tag: [],
-        title: "AWS CloudFormation support",
-        description: "Supporting AWS CloudFormation as an IaC provider",
       },
     ],
   },
