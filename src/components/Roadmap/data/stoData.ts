8d72ff71af52bcc74532c6ef21e2eab41bc91290
Teja Kummarikuntla <teja.kummarikuntla@harness.io>
2025-10-23 19:08:12 +0000

    STO: Update Roadmap (#100220)
    * 9ee26d Update STO Roadmap

--- src/components/Roadmap/data/stoData.ts
+++ src/components/Roadmap/data/stoData.ts
@@ -5,29 +5,9 @@ export const StoData: Horizon = {
     description: "Q3 2025, Aug 2025 - Oct 2025",
     feature: [  
       {
-        tag: [{ value: "Notifications" }, { value: "Platform" }],
-        title: "Exemption Notifications",
-        description: "Notify developers and AppSec teams via email, Slack, Microsoft Teams, or a custom webhook about pending, approved, and expired exemption requests.",
-      },
-      {
         tag: [{ value: "Vulnerability" }, { value: "Governance" }],
         title: "Base image vs App layer vulnerability",
         description: "Ability to see base image vs. app layer vulnerabilities and govern pipelines.",
-      },
-      {
-        tag: [{ value: "Integration" }, { value: "Sonatype"}],
-        title: "Sonatype Integration",
-        description: "Native integration with Sonatype Nexus IQ Server and Nexus Repository.",
-      },
-      {
-        tag: [{ value: "Integration" }, { value: "GHAS"}],
-        title: "GitHub Advanced Security Integration",
-        description: "Native integration with GitHub Advanced Security for CodeQL, SCA, and Secrets.",
-      },
-      {
-        tag: [{ value: "Platform" }, { value: "RBAC" }],
-        title: "Download Issues Data as CSV from Vulnerabilities Tab",
-        description: "Enable RBAC-honoring download of issues CSV from Vulnerabilities Tab, add API, and deep-link to Pipeline Execution Summary Dashboard with execution ID."
       },
       {
         tag: [{ value: "Visibility" }, { value: "AppSec" }],
@@ -38,11 +18,6 @@ export const StoData: Horizon = {
         tag: [{ value: "Visibility" }, { value: "Platform" }],
         title: "Vulnerability overview under pipeline execution page",
         description: "Show vulnerability stats on the Pipeline execution at stage level."
-      },
-      {
-        tag: [{ value: "AI" }, { value: "Visibility" }],
-        title: "AppSec Chatbot",
-        description: "AI chatbot to help with STO use cases."
       },
       {
         tag: [{ value: "Vulnerability Prioritization" }, { value: "AppSec" }],
@@ -60,21 +35,6 @@ export const StoData: Horizon = {
         description: "Aggregate security scan results from all stages/executions including aborted, resumed pipeline executions."
       },
       {
-        tag: [{ value: "Integration" }, { value: "Anchore" }],
-        title: "Anchore Enhancements",
-        description: "Update the Anchore runner to the latest version, add advanced configuration, and more."
-      },          
-      {
-        tag: [{ value: "Integration" }, { value: "SonarQube" }],
-        title: "SonarQube: Code/Bug Smells as Vulnerabilities",
-        description: "Treat SonarQube Code/Bug Smells as vulnerabilities instead of info-level issues."
-      },
-      {
-        tag: [{ value: "Visibility" }, { value: "Platform" }],
-        title: "Deduplication Stats",
-        description: "Show deduplication stats at the pipeline level."
-      },
-      {
         tag: [{ value: "Integration" }, { value: "ServiceNow" }],
         title: "Exemption Management via ServiceNow",
         description: "Native ServiceNow integration for exemption management.",
@@ -83,11 +43,6 @@ export const StoData: Horizon = {
         tag: [{ value: "EPSS" }],
         title: "Exploit Protection Scoring System (EPSS)",
         description: "Provide EPSS score in addition to CVSS score for better vulnerability prioritization.",
-      },
-      {
-        tag: [{ value: "Visibility" }, { value: "Platform" }],
-        title: "Expose Internal Issue ID",
-        description: "Show STO internal issue ID under the issue details."
       }
     ],
   },
@@ -155,6 +110,58 @@ export const StoData: Horizon = {
   Released: {
     description: "What has been released",
     feature: [
+      {
+        tag: [{ value: "Notifications" }, { value: "Platform" }],
+        title: "Exemption Notifications",
+        description: "Notify developers and AppSec teams via email, Slack, Microsoft Teams, or a custom webhook about pending, approved, and expired exemption requests.",
+        link: "https://developer.harness.io/docs/security-testing-orchestration/notifications/exemption-notifications/"
+      },
+      {
+        tag: [{ value: "Integration" }, { value: "GHAS"}],
+        title: "GitHub Advanced Security Integration",
+        description: "Native integration with GitHub Advanced Security for CodeQL, SCA, and Secrets.",
+        link: "https://developer.harness.io/docs/security-testing-orchestration/sto-techref-category/github-advanced-security/"
+      },
+      {
+        tag: [{ value: "Platform" }, { value: "RBAC" }],
+        title: "Download Issues Data as CSV from Vulnerabilities Tab",
+        description: "Enable RBAC-honoring download of issues CSV from Vulnerabilities Tab, add API, and deep-link to Pipeline Execution Summary Dashboard with execution ID.",
+        link: "https://developer.harness.io/docs/security-testing-orchestration/view-security-test-results/export-scan-results/"
+      },
+      {
+        tag: [{ value: "Integration" }, { value: "Sonatype"}],
+        title: "Sonatype Integration",
+        description: "Native integration with Sonatype Nexus IQ Server and Nexus Repository.",
+        link: "https://developer.harness.io/docs/security-testing-orchestration/sto-techref-category/nexus-scanner-reference/"
+      },
+      {
+        tag: [{ value: "Visibility" }, { value: "Platform" }],
+        title: "Deduplication Stats",
+        description: "Show deduplication stats at the pipeline level.",
+        link: "https://developer.harness.io/docs/security-testing-orchestration/view-security-test-results/view-scan-results"
+      },
+      {
+        tag: [{ value: "Integration" }, { value: "Anchore" }],
+        title: "Anchore Enhancements",
+        description: "Update the Anchore runner to the latest version, add advanced configuration, and more.",
+        link: "https://developer.harness.io/docs/security-testing-orchestration/sto-techref-category/anchore-enterprise-scanner-reference/"
+      },
+      {
+        tag: [{ value: "Visibility" }, { value: "Platform" }],
+        title: "Expose Internal Issue ID",
+        description: "Show STO internal issue ID under the issue details."
+      },
+      {
+        tag: [{ value: "Integration" }, { value: "SonarQube" }],
+        title: "SonarQube: Code/Bug Smells as Vulnerabilities",
+        description: "Treat SonarQube Code/Bug Smells as vulnerabilities instead of info-level issues.",
+        link: "https://developer.harness.io/docs/security-testing-orchestration/sto-techref-category/sonarqube-sonar-scanner-reference/"
+      },
+      {
+        tag: [{ value: "AI" }, { value: "Visibility" }],
+        title: "AppSec Chatbot",
+        description: "AI chatbot to help with STO use cases."
+      },
       {
         tag: [{ value: "Visibility" }, { value: "AppSec" }],
         title: "Issues List - Project",
