4f58a98cb8cb572bd3a99e178bc7ae2e098440e4
Lavakush Biyani <lavakush.biyani@harness.io>
2025-11-18 03:32:45 +0000

    SCS: Q4 Roadmap Updation (#100404)
    * 3b2193 modify Q1 to Q2
    
    * 694701 Update Q4 roadmap
    
    * 1ff9c8 SCS: Q4 Roadmap Updation

--- src/components/Roadmap/data/sscaData.ts
+++ src/components/Roadmap/data/sscaData.ts
@@ -3,78 +3,90 @@ import { Horizon } from "./roadmapData";
 export const SscaData: Horizon = {
   
   "Now": {
-    description: "Q3 2025, August 2025 - October 2025",
+    description: "Q4 2025, November 2025 - January 2026",
     feature: [
       // {
       //   tag: [{value: "Artifact Security"}],
       //   title: "Non-Container based Artifact Signing & Verification",
       //   description: "Support for signing and verification for non-containerized artifacts like helm charts, manifest files, JARs, WARs, etc.",
       // },
+      // {
+      //    tag: [{value: "SBOM"}],
+      //    title: "SBOM Ingestion for Non-Containerized Artifacts",
+      //    description: "Enable ingestion of SBOMs for artifacts such as WAR, JAR, and other non-containerized files.",
+      // },
       {
-         tag: [{value: "SBOM"}],
-         title: "SBOM Ingestion for Non-Containerized Artifacts",
-         description: "Enable ingestion of SBOMs for artifacts such as WAR, JAR, and other non-containerized files.",
+         tag: [{value: "Governance"}, {value : "Attestation"}],
+         title: "Custom Attestation Framework",
+         description: "Ingest your own custom attestations and leverage Harness’s querying and policy engine to enforce governance and compliance across all pipelines and artifacts.",
       },
-      {
-        tag: [{value : "Governance"}, {value: "Audit Trail"}],
-        title: "Artifact Chain of Custody v2",
-        description: "Enhanced audit trail that seamlessly integrates all pipeline events at an account level, spanning from source code to deployment.",
-      },
-      {
-        tag: [{value: "AI"}],
-        title: "AI Chatbot",
-        description: "AI-powered chatbot capabilities to assist users within the SCS module",
-      },
-      {
-        tag: [{value : "Risk & Compliance"}, {value : "OWASP"}],
-        title: "OWASP OSS Top 10 Risks",
-        description: "Visibility into End of Life components using SBOMs.",
-      },
+      // {
+      //   tag: [{value : "Risk & Compliance"}, {value : "OWASP"}],
+      //   title: "OWASP OSS Top 10 Risks",
+      //   description: "Visibility into End of Life components using SBOMs.",
+      // },
       {
         tag: [{value: "Artifact Security"}],
         title: "Keyless attestation/signing support",
         description: "Support SBOM and SLSA attestations, as well as artifact signing, using keyless workflows.",
       },
-      {
-        tag: [{value : "Risk & Compliance"}],
-        title: "Global Artifact & Repository visibility",
-        description: "Account-wide views of repositories and artifacts, enabling unified visibility and oversight across all resources.",
-      },
        {
         tag: [{value: "Dependency Management"}],
-        title: "Automate OSS Dependency Updates",
-        description: "Automatically generate PRs for updating outdated dependencies.",
+        title: "Automate OSS Dependency Updates with Harness AI",
+        description: "Leverage Harness AI to automatically generate PRs for updating outdated dependencies.",
       },
+      {
+        tag: [{value: "SBOM"}],
+        title: "SBOM Direct and Indirect Dependencies",
+        description: "Gain full visibility into OSS usage in your code repos by analyzing both direct and transitive dependencies for comprehensive risk insights.",
+      },
+      {
+        tag: [{value: "AI"}],
+        title: "Agentic AI Workflows",
+        description: "Deliver intelligent insights and automate key SCS actions to proactively secure your software supply chain with AI-driven agents.",
+      },
+
     
     ],
   },
   "Next": {
-    description: "Q4 2025, November 2025 - January 2026",
+    description: "Q1 2026, February 2026 - April 2026",
     feature: [
       {
         tag: [{value: "Integration"}, {value : "Artifact Security"}],
         title: "Cosign AWS Support",
         description: "Leverage keys from AWS KMS to sign and verify artifacts.",
       },
+      {
+        tag: [{value: "Integration"},{value: "Repo Security"}],
+        title: "Support for Gitlab & Bitbucket",
+        description: "Complete support for GitLab and Bitbucket, allowing users to onboard repositories and perform configuration checks, SBOM generation, and security scans.",
+      },
+      {
+        tag: [{value : "Governance"}, {value: "Audit Trail"}],
+        title: "Artifact Chain of Custody v2",
+        description: "Enhanced audit trail that seamlessly integrates all pipeline events at an account level, spanning from source code to deployment.",
+      },
       // {
       //   tag: [{value: "Visibility"}],
       //   title: "Global Level View",
       //   description: "Gain complete visibility into all artifact and code repositories across projects, along with their associated findings, in a unified account-level view.",
       // },
-      {
-         tag: [{value: "UX"}],
-         title: "UX Enhancements",
-         description: "Improving search, filtering across product pages and overall user experience",
-      },
-      {
-          tag: [{value : "SLSA"}, {value: "Artifact Security"}],
-          title: "SLSA Policies",
-          description: "Out of the box policies to ensure compliance with Level 1, Level 2, and Level 3 requirements.",
-      },
+      // {
+      //    tag: [{value: "UX"}],
+      //    title: "UX Enhancements",
+      //    description: "Improving search, filtering across product pages and overall user experience",
+      // },
+
       {
         tag: [{value : "Repo Security"}],
         title: "Repo Security Posture Management for Harness Code",
         description: "Identify misconfigurations in source code repositories based on industry standards such as CIS v1.0 and OWASP Top 10 CI/CD Risk. Also, includes support for SBOM generation and security tests such as SAST, SCA, and secrets scanning.",
+      },
+      {
+        tag: [{value : "Risk & Compliance"}],
+        title: "Global Artifact & Repository visibility",
+        description: "Account-wide views of repositories and artifacts, enabling unified visibility and oversight across all resources.",
       },
       {
         tag: [{value: "Run time Security"}],
@@ -90,12 +102,17 @@ export const SscaData: Horizon = {
     ],
   },
   "Later": {
-    description: "Q1 2026+, February 2026 & beyond",
+    description: "Q2 2026+, May 2026 & beyond",
     feature: [
       {
         tag: [{value : "CI/CD Security"}],
         title: "CI/CD Security for Jenkins",
         description: "Perform static analysis to detect risks and misconfigurations in Jenkins pipelines.",
+      },
+      {
+        tag: [{value : "AIBOM"}],
+        title: "AIBOM",
+        description: "Gain visibility into all AI models, datasets, and prompts used across your systems, enabling governance, risk assessment, and secure AI adoption.",
       },
       {
         tag: [{value : "SBOM"}, {value : "SLSA"}, {value: "CI/CD Security"}],
@@ -115,13 +132,13 @@ export const SscaData: Horizon = {
        {
         tag: [{value: "Integration"}, {value : "OpenSSF"}],
         title: "OpenSSF Integration",
-        description: "Support for OpenSSF Rules.",
+        description: "Enforce industry-standard OpenSSF rules to strengthen build integrity, dependency hygiene, and overall software supply chain security.",
       },
-      {
-        tag: [{value: "Integration"},{value: "Repo Security"}],
-        title: "Support for Gitlab & Bitbucket",
-        description: "Complete support for GitLab and Bitbucket, allowing users to onboard repositories and perform configuration checks, SBOM generation, and security scans.",
-      },
+      // {
+      //   tag: [{value: "Integration"},{value: "Repo Security"}],
+      //   title: "Support for Gitlab & Bitbucket",
+      //   description: "Complete support for GitLab and Bitbucket, allowing users to onboard repositories and perform configuration checks, SBOM generation, and security scans.",
+      // },
       // {
       //   tag: [{value : "Remediation"}, {value: "AppSec"}],
       //   title: "Remediation Tracker",
@@ -132,6 +149,13 @@ export const SscaData: Horizon = {
         title: "Exemption Management",
         description: "Manage exemptions for risk and compliance issues across all targets (Artifacts, CI/CD, Repos).",
       },
+      {
+          tag: [{value : "SLSA"}, {value: "Artifact Security"}],
+          title: "SLSA Policies",
+          description: "Out of the box policies to ensure compliance with Level 1, Level 2, and Level 3 requirements.",
+      },
+
+      
     ],
   },
   "Released": {
@@ -139,7 +163,7 @@ export const SscaData: Horizon = {
     feature: [
       {
          tag: [{value : "Risk & Compliance"}],
-         title: "OWASP OSS Top 10 Risks (Beta)",
+         title: "OWASP OSS Top 10 Risks",
          description: "Visibility into outdated and unmaintained components using SBOMs"
       },
       {
@@ -172,15 +196,20 @@ export const SscaData: Horizon = {
         title: "Artifact Signing and Verification (Containers & Non-Containers)",
         description: "Ensure built artifacts are not tampered before deployment.",
       },
+      // {
+      //   tag: [{value : "Risk & Compliance"}],
+      //   title: "Report Generation",
+      //   description: "Generate comprehensive license reports detailing the licenses associated with artifacts.",
+      // },
+      // {
+      //   tag: [{value: "API"}],
+      //   title: "API Support",
+      //   description: "Enable bulk onboarding of GitHub repos across orgs and accounts, and provide SBOM download APIs for repos and artifacts."
+      // },
       {
-        tag: [{value : "Risk & Compliance"}],
-        title: "Report Generation",
-        description: "Generate comprehensive license reports detailing the licenses associated with artifacts.",
-      },
-      {
-        tag: [{value: "API"}],
-        title: "API Support",
-        description: "Enable bulk onboarding of GitHub repos across orgs and accounts, and provide SBOM download APIs for repos and artifacts."
+        tag: [{value: "AI"}],
+        title: "AI Chatbot(Beta)",
+        description: "AI-powered chatbot capabilities to assist users within the SCS module",
       },
     ],
   },
