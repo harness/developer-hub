48d809708b712216b9f2fa6df5060cc0a57aed06
Richard Black <richard.black@harness.io>
2026-08-20 12:54:29 +0000

    chore: [HDH-1431]: Update IaCM API spec (#103247)
    * 408dd7 HDH-1431 Update IaCM API spec

--- src/components/ApiReference/modules/infra-as-code-management/config.ts
+++ src/components/ApiReference/modules/infra-as-code-management/config.ts
@@ -2,7 +2,7 @@ import type { ApiReferenceModuleConfig } from '../types';
 
 const config: ApiReferenceModuleConfig = {
   name: 'Infrastructure as Code Management',
-  specUrl: 'https://app.harness.io/prod1/iacm/openapi3.yaml',
+  specUrl: 'https://qa.harness.io/iacm/openapi3.yaml',
   specFormat: 'yaml',
   pathPrefix: '/iacm',
   sidebarIconClass: 'sidebar-iacm',
